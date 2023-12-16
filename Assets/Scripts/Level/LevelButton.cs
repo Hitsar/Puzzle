a2f75bf70802227409de182b2bd79487e09573a4
Hitsar <106964982+Hitsar@users.noreply.github.com>
2023-12-16 19:09:00 +0300

    чистка

--- Assets/Scripts/Level/LevelButton.cs
+++ Assets/Scripts/Level/LevelButton.cs
@@ -1,18 +1,18 @@
 using Shop;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Level
 {
     public class LevelButton : MonoBehaviour
     {
         [SerializeField] private int _level;
-        [SerializeField] private PuzzleFormat _format;
         [SerializeField] private int _moneyForWin;
 
         public virtual void LoadLevel()
         {
             FindAnyObjectByType<Wallet>().SetMoneyForWin(_moneyForWin);
-            FindAnyObjectByType<LevelsController>().LoadLevel(_level, _format);
+            SceneManager.LoadScene(_level);
         }
     }
 }
--- Assets/Scripts/Shop/Wallet.cs
+++ Assets/Scripts/Shop/Wallet.cs
@@ -8,15 +8,6 @@ namespace Shop
         [SerializeField] private TMP_Text _text;
         private int _money;
         private int _moneyForWin;
-        
-        public bool AddMoney(int money)
-        {
-            if (money <= 0) return false;
-            
-            _money += money;
-            _text.text = _money.ToString();
-            return true;
-        }
 
         public void AddMoneyForWin()
         {
