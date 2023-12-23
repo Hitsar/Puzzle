64cc71925b381fd948441243d5c0b241432bb5ba
GORILA1221 <kkudashev2018@gmail.com>
2023-12-23 22:42:50 +0500

    Черновая локализация

---
+++ Assets/Scripts/Translation/LanguageManager.cs
@@ -0,0 +1,14 @@
+namespace YG
+{
+    // —татический класс дл€ управлени€ сменой €зыка
+    public static class LanguageManager
+    {
+        public delegate void LanguageChangeHandler(string newLanguage);
+        public static event LanguageChangeHandler OnLanguageChanged;
+
+        public static void ChangeLanguage(string language)
+        {
+            OnLanguageChanged?.Invoke(language);
+        }
+    }
+}
