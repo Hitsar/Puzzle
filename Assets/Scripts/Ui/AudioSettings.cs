2e972e755cb4b351f5ca2e6c6d31bb2fce63d6f5
Hitsar <106964982+Hitsar@users.noreply.github.com>
2023-12-30 11:22:18 +0300

    исправил визуальный баг звуков

---
+++ Assets/Scripts/Ui/AudioSettings.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Ui
+{
+    public class AudioSettings : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer _audioMixer;
+        [SerializeField] private Toggle _musicToggle, _soundToggle;
+
+        private void Start()
+        {
+            _musicToggle.onValueChanged.AddListener(SetMutedMusic);
+            _soundToggle.onValueChanged.AddListener(SetMutedSound);
+
+            if (_audioMixer.GetFloat("Music", out float volumeMusic) && volumeMusic < 0) _musicToggle.isOn = true;
+            if (_audioMixer.GetFloat("Sound", out float volumeSound) && volumeSound < 0) _soundToggle.isOn = true;
+        }
+
+        private void SetMutedMusic(bool isMuted)
+        {
+            if (isMuted) _audioMixer.SetFloat("Music", -80);
+            else _audioMixer.SetFloat("Music", 0);
+        }
+        
+        private void SetMutedSound(bool isMuted)
+        {
+            if (isMuted) _audioMixer.SetFloat("Sound", -80);
+            else _audioMixer.SetFloat("Sound", 0);
+        }
+    }
+}
