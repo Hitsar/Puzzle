47d063c5da6c830aedd9882eebdd6a32342f24cc
torn2706 <torn0627@gmail.com>
2024-01-10 14:04:13 +0100

    Unchecked AllowRotation in atlases, simplified logic of puzzle placer

--- Assets/Scripts/PuzzleBuilder/PuzzlePlacer.cs
+++ Assets/Scripts/PuzzleBuilder/PuzzlePlacer.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections.Generic;
 using Zenject;
 
@@ -16,8 +15,6 @@ namespace PuzzleBuilder
         private Vector2 _currentPiecePosition = Vector2.zero;
         private const float _aproximator = 1f;
         private Vector2 _currentPieceNumber = new Vector2(1,0);
-        private float _previousBottomOffset;
-        private Part _prevoiusPartY;
         private List<Part> _prevoiusPartsX = new List<Part>();
         private List<Part> _parts = new List<Part>();
         private List<float> _previousOffsets = new List<float>();
@@ -27,9 +24,6 @@ namespace PuzzleBuilder
         {
             float convexOffsetX = 0f;
             float convexOffsetY = 0f;
-            float bottomOffset = 0f;
-
-            Part currentPartY;
 
             _currentPieceNumber = new Vector2(_currentPieceNumber.x, _currentPieceNumber.y + 1);
             if (_currentPieceNumber.y > dimensionalSize.y)
@@ -42,21 +36,15 @@ namespace PuzzleBuilder
                 _currentPiecePosition = new Vector2(_currentPiecePosition.x + minMaxSize.x, 0f);
 
                 _prevoiusPartsX.Clear();
-                foreach(Part part in _parts)
-                {
-                    _prevoiusPartsX.Add(part);
-                }
+                _prevoiusPartsX.AddRange(_parts);
                 _parts.Clear();
 
                 _previousOffsets.Clear();
-                foreach (float offset in _offsets)
-                {
-                    _previousOffsets.Add(offset);
-                }
+                _previousOffsets.AddRange(_offsets);
                 _offsets.Clear();
             }
-            //Find x part format
 
+            //Find x part format
             if (rectTransform.sizeDelta.x - _aproximator <= minMaxSize.x)
             {
                 _parts.Add(Part.min);
@@ -87,63 +75,23 @@ namespace PuzzleBuilder
                     else if( _prevoiusPartsX[_parts.Count - 1] == Part.middle)
                     {
                         convexOffsetX = 0f;
-
                         if (_previousOffsets[_parts.Count - 1] != 0)
                             convexOffsetX = convexSize;
                     }
                 }
             }
 
-            //Find y part format
-            if (rectTransform.sizeDelta.y - _aproximator <= minMaxSize.x)
-            {
-                currentPartY = Part.min;
-                convexOffsetY = 0f;
-                _currentPiecePosition = _currentPiecePosition - new Vector2(0, rectTransform.sizeDelta.y - convexOffsetY);
+            if (_currentPieceNumber.y > 1)
+                convexOffsetY = convexSize;
 
-                if (_currentPieceNumber.y != 1)
-                {
-                    _currentPiecePosition = _currentPiecePosition + new Vector2(0, _previousBottomOffset);
-
-                    if (_prevoiusPartY == Part.middle)
-                        _currentPiecePosition = _currentPiecePosition + new Vector2(0, convexSize);
-                }
+            float positionOffset = 0f;
+            if (_currentPieceNumber.y > 2)
+                positionOffset = convexOffsetY;
                 
-            }
-            else if (rectTransform.sizeDelta.y + _aproximator >= minMaxSize.y)
-            {
-                //Debug.Log("Max part y");
-                currentPartY = Part.max;
-                convexOffsetY = convexSize;
-                bottomOffset = convexSize;
-                _currentPiecePosition = _currentPiecePosition - new Vector2(0, rectTransform.sizeDelta.y - convexOffsetY);
-            }
-            else
-            {
-                currentPartY = Part.middle;
-                //Debug.Log("MiddlePart y");
-                convexOffsetY = convexSize;
+            _currentPiecePosition = _currentPiecePosition - new Vector2(0, rectTransform.sizeDelta.y - positionOffset);
 
-                float topOffset = 0f;
-                if (_currentPieceNumber.y == 1f)
-                    topOffset = convexSize;
-
-                _currentPiecePosition = _currentPiecePosition - new Vector2(0, rectTransform.sizeDelta.y - convexOffsetY + topOffset);
-
-                if (_currentPieceNumber.y != 1)
-                {
-                    _currentPiecePosition = _currentPiecePosition + new Vector2(0, _previousBottomOffset);
-
-                    if (_prevoiusPartY == Part.max)
-                        _currentPiecePosition = _currentPiecePosition - new Vector2(0, convexSize);
-                }
-                    
-            }
-
-            rectTransform.anchoredPosition = new Vector2(_currentPiecePosition.x - rectTransform.sizeDelta.x / 2 + convexOffsetX, _currentPiecePosition.y + rectTransform.sizeDelta.y / 2);
-            _previousBottomOffset = bottomOffset;
+            rectTransform.anchoredPosition = new Vector2(_currentPiecePosition.x - rectTransform.sizeDelta.x / 2 + convexOffsetX, _currentPiecePosition.y + rectTransform.sizeDelta.y / 2 + convexOffsetY);
             _offsets.Add(convexOffsetX);
-            _prevoiusPartY = currentPartY;
         }
 
         public enum Part
